892aef076ed9e0019dc32b05a5a5ee3a6ca6c204
Morteza <m.golalipour@yahoo.com>
2021-07-11 13:09:42 +0430

    profile-address-postal_code

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,8 @@ app_name = 'accounts'
 urlpatterns = [
     path('verify/',views.verify,name='verify'),
     path('register/',views.register_view,name='register_view'),
+    path('profile/',views.profile,name='profile'),
+    path('profile/address/',views.address,name='address')
 
 
 ]
