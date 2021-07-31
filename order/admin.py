5446547e7c3163894b555374033c5d610f2e0423
mrrobot <m.golalipour@yahoo.com>
2021-08-01 03:30:22 +0430

    d

--- order/admin.py
+++ order/admin.py
@@ -4,15 +4,17 @@ from .models import *
 
 class ItemInline(admin.TabularInline):
     model = ItemOrder
-    readonly_fields = ['user','product','variant','size','color','quantity']
+    readonly_fields = ['user','product','variant','size','color','quantity','price']
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['user','f_name','l_name','address','phone','postal_code2','paid','detail']
+    list_display = ['user','f_name','l_name','phone','address','postal_code2','get_price','paid','detail']
     inlines = [ItemInline]
 
-
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['code','start','end','discount','active']
 
 
 
 admin.site.register(Order,OrderAdmin)
 admin.site.register(ItemOrder)
+admin.site.register(Coupon,CouponAdmin)
