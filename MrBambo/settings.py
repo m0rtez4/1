2a9f53bbc53a16acf88bdf81b2db96a42a0920ea
m0rtez4 <88965967+m0rtez4@users.noreply.github.com>
2021-08-15 15:45:04 +0430

    Update settings.py

--- MrBambo/settings.py
+++ MrBambo/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django1',
         'USER': 'postgres',
-        'PASSWORD': 'M@8663teza',
+        'PASSWORD': '123',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
